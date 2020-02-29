4bb5754808fc0f758ae594cf01434753292ac69a
howard-yen <howardy2000@gmail.com>
2020-02-29 14:13:23 +0000

    changed attack

--- python-algo/algo_strategy.py
+++ python-algo/algo_strategy.py
@@ -84,34 +84,23 @@ class AlgoStrategy(gamelib.AlgoCore):
         else:
             # Now let's analyze the enemy base to see where their defenses are concentrated.
             # If they have many units in the front we can build a line for our EMPs to attack them at long range.
-            if self.detect_enemy_unit(game_state, unit_type=None, valid_x=None, valid_y=[14, 15]) > 10:
-                self.emp_line_strategy(game_state)
-            else:
-                # They don't have many units in the front so lets figure out their least defended area and send Pings there.
 
-                # Only spawn Ping's every other turn
-                # Sending more at once is better since attacks can only hit a single ping at a time
-                if game_state.turn_number % 2 == 1:
-                    # To simplify we will just check sending them from back left and right
-                    index = random.randrange(4);
-                    ping_spawn_location_options = [[8, 5], [7, 6], [20, 6], [19, 5]]
-                    # best_location = self.least_damage_spawn_location(game_state, ping_spawn_location_options)
-                    best_location = ping_spawn_location_options[index]
-                    game_state.attempt_spawn(PING, best_location, 2)
-                    if best_location[0] < 14:
-                        game_state.attempt_spawn(EMP, [best_location[0]+1, best_location[1]-1], 1)
-                    else:
-                        game_state.attempt_spawn(EMP, [best_location[0]-1, best_location[1]-1], 1)
-                    game_state.attempt_spawn(PING, best_location, 1000)
+            # They don't have many units in the front so lets figure out their least defended area and send Pings there.
+            # Only spawn Ping's every other turn
+            # Sending more at once is better since attacks can only hit a single ping at a time
+            if game_state.turn_number % 3 == 2:
+                # To simplify we will just check sending them from back left and right
+                build_emp_ping_combo(game_state)
 
-                # Lastly, if we have spare cores, let's build some Encryptors to boost our Pings' health.
-                encryptor_locations = [[6, 9], [6, 10], [7, 9], [7, 10]]
-                game_state.attempt_spawn(ENCRYPTOR, encryptor_locations)
+            # Lastly, if we have spare cores, let's build some Encryptors to boost our Pings' health.
+            encryptor_locations = [[6, 9], [6, 10], [7, 9], [7, 10]]
+            game_state.attempt_spawn(ENCRYPTOR, encryptor_locations)
 
     def build_emp_ping_combo(self, game_state):
         # build emp one to the right and up of the pings
-        ping_spawn_location_options = [[8, 5], [7, 6], [20, 6], [19, 5]]
-        best_location = self.least_damage_spawn_location(game_state, ping_spawn_location_options)
+        ping_spawn_location_options = [[8, 5], [7, 6], [22, 8], [23, 9]]
+        index = random.randrange(4);
+        best_location = ping_spawn_location_options[index]
         count = game_state.get_resource(BITS) // 13;
         if count == 0:
             count = 1
@@ -120,8 +109,10 @@ class AlgoStrategy(gamelib.AlgoCore):
             game_state.attempt_spawn(EMP, [best_location[0]+1, best_location[1]-1], count)
         else:
             game_state.attempt_spawn(PING, best_location, game_state.get_resource(BITS) - 3 * count)
-            game_state.attempt_spawn(EMP, [best_location[0]-1, best_location[1]-1], count)
+            game_state.attempt_spawn(EMP, [best_location[0]+1, best_location[1]+1], count)
 
+    def is_successful(game_state):
+        s = "something"
 
     def build_defences(self, game_state):
         """
